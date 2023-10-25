ddfd0d37f7636a315c12e42ac45f7197c9fc86cd
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2023-10-25 22:32:50 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,77 +1,78 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
-#include <cstring>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> a[1001]; // 인접리스트를 이용
-bool check[1001]; // 정점에 방문했는지를 체크하는 배열
+int N, M, V;
+vector< vector<int> > map;
+int visited[1001] = { 0 };
 
-void dfs(int node) {
+void DFS(int now) {
+	cout << now << " ";
 
-	check[node] = true; // 첫 방문
-	printf("%d ", node);
-
-	for (int i = 0; i < a[node].size(); i++) { // 저장된 인접리스트의 원소들에 대해 각각 검사
-		int next = a[node][i];
-
-		if (check[next] == false) {
-			dfs(next);
-		}
+	// 현재 노드와 연결된 노드 찾기
+	for (int i = 0; i < map[now].size(); i++) {
+		int next = map[now][i];
+		if (visited[next] == 1) continue;
+		visited[next] = 1;
+		DFS(next);
 	}
 
 }
 
-void bfs(int start) {
-	queue<int> q; // bfs는 큐 이용
+void BFS(int start) {
+	queue<int> qu;
 
-	memset(check, false, sizeof(check));
+	visited[V] = 1;
+	qu.push(start);
 
-	check[start] = true; 
-	q.push(start); // 첫 노드를 큐에 추가
+	while (!qu.empty()) {
+		int now = qu.front();
+		qu.pop();
 
-	while (!q.empty()) { // 큐가 빌때까지 반복
-		int node = q.front(); // 큐의 처음 원소 검사
-		q.pop();
+		cout << now << " ";
 
-		printf("%d ", node);
-
-		for (int i = 0; i < a[node].size(); i++) {
-			int next = a[node][i];
-			if (check[next] == false) {
-				check[next] = true; // 큐에 넣을때, 방문한것으로 취급!
-				q.push(next);
-			}
+		for (int i = 0; i < map[now].size(); i++) {
+			int next = map[now][i];
+			if (visited[next] == 1) continue;
+			visited[next] = 1;
+			qu.push(next);
 		}
+
 	}
+
 
 }
 
 int main() {
+	// freopen_s(new FILE*, "input.txt", "r", stdin);
 
-	int n, m, start;
+	cin >> N >> M >> V;
 
-	scanf("%d %d %d", &n, &m, &start);
+	map.resize(N + 1);
 
-	for (int i = 0; i < m; i++) {
-		int u, v;
-
-		scanf("%d %d", &u, &v);
-
-		a[u].push_back(v); // (u,v)
-		a[v].push_back(u); // (v,u)
+	while (M--) {
+		int start, end;
+		cin >> start >> end;
+		map[start].push_back(end);
+		map[end].push_back(start);
 	}
 
-	// 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문 
-	for (int i = 1; i <= n; i++) {
-		sort(a[i].begin(), a[i].end());
+	for (int i = 0; i < N + 1; i++) {
+		sort(map[i].begin(), map[i].end());
 	}
+	
+	
+	
+	visited[V] = 1;
+	DFS(V);
+	cout << "\n";
 
-	dfs(start);
-	printf("\n");
-	bfs(start);
-	printf("\n");
+	fill(visited, visited + 1001, 0);
+
+	BFS(V);
+
 	return 0;
 }
