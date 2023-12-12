01ce4c79ca9910cbbba14ef248e740aba694ed60
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2023-12-12 21:39:12 +0900

    [Silver II] Title: 결혼식, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.cc
+++ 백준/Silver/5567. 결혼식/결혼식.cc
@@ -1,55 +1,45 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
-#define MAX 501
-
 int n, m;
-int map[MAX][MAX]; // 1 ~ n개 사용
-int visited[MAX];
+vector< vector<int> > map;
+int visited[501];
 
-struct node {
-	int num;
-	int level;
-};
+void DFS(int level, int now) {
+	if (level >= 2) {
+		return;
+	}
+
+	for (int i = 0; i < map[now].size(); i++) {
+		int next = map[now][i];
+		visited[next] = 1;
+		DFS(level + 1, next);
+	}
+}
 
 int main() {
 	// freopen_s(new FILE*, "input.txt", "r", stdin);
 	cin >> n >> m;
+	map.resize(n+1);
 
-	// m개 입력
 	for (int i = 0; i < m; i++) {
 		int start, end;
 		cin >> start >> end;
-		map[start][end] = 1; // 양방향 노드
-		map[end][start] = 1;
+		map[start].push_back(end);
+		map[end].push_back(start);
 	}
 
-	visited[1] = 1; // 상근이
-	queue<node> qu;
-	qu.push({ 1, 0 });
-
-	while (!qu.empty()) {
-		node now = qu.front();
-		qu.pop();
-
-		for (int i = 2; i <= n; i++) {
-			if (now.level >= 2) continue; // 친구의 친구까지만 살펴봄
-			if (visited[i] == 1) continue; // 이미 초대한 경우
-			if (map[now.num][i] == 0) continue; // 친구가 아닌 경우
-
-			visited[i] = 1;
-			qu.push({ i, now.level + 1 });
-		}
-	}
+	visited[1] = 1;
+	DFS(0, 1);
 
 	int cnt = 0;
 	for (int i = 2; i <= n; i++) {
-		if (visited[i] == 1) {
+		if (visited[i] == 1)
 			cnt++;
-		}
 	}
+
 	cout << cnt;
 	return 0;
 }
