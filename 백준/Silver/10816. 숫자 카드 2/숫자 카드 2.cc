27daf688b89903095c3dd4240fee7d399d305917
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2023-12-13 17:07:16 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,29 +1,31 @@
 #include <iostream>
-#include <unordered_map>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int N, M;
 int card;
+vector<int> arr;
 
 int main() {
-    ios_base::sync_with_stdio(false);
+    // freopen_s(new FILE*, "input.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	// freopen_s(new FILE*, "input.txt", "r", stdin);
-	cin >> N;
-	
-	unordered_map<int, int> um;
 
+	cin >> N;
+	arr.resize(N);
 	for (int i = 0; i < N; i++) {
 		cin >> card;
-		um[card]++; // 이렇게 간단히 추가할 수 잇음
+		arr[i] = card;
 	}
+
+	sort(arr.begin(), arr.end());
 
 	cin >> M;
 	while (M--) {
 		cin >> card;
-		cout << um[card] << " ";
+		cout << upper_bound(arr.begin(), arr.end(), card) - lower_bound(arr.begin(), arr.end(), card) << " ";
 	}
 
 	return 0;
