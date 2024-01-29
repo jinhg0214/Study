bf0475861b3eb62f119118ece6b9549f074dc96e
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2024-01-29 18:09:01 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -1,45 +1,21 @@
-/*
-브2 : 수학은 비대면 강의 
-연립 방정식의 해를 구하는 문제
-
-ax + by = c
-dx + ey = f
-
-a, b, c, d, e, f 가 주어질 때, (-999 <= a,b,c,d,e,f <= 999)
-
-유일한 해의 쌍 x, y를 구하는 문제. (-999 <= x, y <= 999, x, y는 정수)
-
-연립 방정식의 해를 구하는 방법은?
-1) 대입법 : 한 방정식을 어느 한 미지수에 대하여 풀고, 그 식을 다른 한 방정식에 대입하여 푼다
-2) 가감법 : 합 또는 차를 이용해서 푼다
-여기선 대입법을 이용함
-
-y = (c - ax)/b 를 2번 식에 대입
-dx + e * (c-ax)/b = f
-를 x에 대하여 정리하면,
-x = (f - e*(c-ax)/b ) / d 가 된다.
-
-필요 변수들
-a, b, c, d, e, f 
-x, y;
-
-*/
-
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	// freopen_s(new FILE*, "sample_input.txt", "r", stdin);
-
 	int a, b, c, d, e, f;
+	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
 
-	cin >> a >> b >> c >> d >> e >> f;
-
-	double x = ((c * e - b * f) / (a * e - b * d));
-	double y = ((-c * d + a * f) / (a * e - b * d));
-
-	cout << x << " " << y;
-
+	for (int x = -999; x < 1000; x++) {
+		for (int y = -999; y < 1000; y++) {
+			if ((a * x + b * y == c) && (d * x + e * y == f)) {
+				printf("%d %d", x, y);
+				return 0;
+			}
+		}
+	}
+	// y를 x에 대해서 정리하여 푸는 방법도 있지마
+	// 시간복잡도가 2000 * 2000 = 4, 000, 000 정도로 1초 내에 계산 가능하므로
+	// 완전탐색으로 풀이
 	return 0;
 }
