93bd7b0ba3a25553fd9dae1735c73491ede27550
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2024-05-13 21:19:16 +0900

    [Silver II] Title: 조합 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.cc
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.cc
@@ -2,35 +2,27 @@
 
 using namespace std;
 
-long long d2(int n) {
-	long long int cnt = 0;
+int counting(int n, int k) {
+	int cnt = 0;
 
-	for (long long int i = 2; i <= n; i *= 2) {
+	for (long long int i = k; i <= n; i = i * k) {
 		cnt += n / i;
 	}
+
 	return cnt;
 }
 
-long long d5(int n) {
-	long long int cnt = 0;
-
-	for (long long int i = 5; i <= n; i *= 5) {
-		cnt += n / i;
-	}
-	return cnt;
-}
 
 int main() {
-	// freopen_s(new FILE*, "input.txt", "r", stdin);
-	int N, M;
+	int N, M; // N, M <= 20억 int +-21억
+
 	cin >> N >> M;
 
-	int de = -1;
-
-	long long int five = d5(N) - d5(N - M) - d5(M);
-	long long int two = d2(N) - d2(N - M) - d2(M);
+	int five = counting(N, 5) - (counting(N - M, 5) + counting(M, 5));
+	int two = counting(N, 2) - (counting(N - M, 2) + counting(M, 2));
 
 	cout << min(five, two);
+
 
 	return 0;
 }
