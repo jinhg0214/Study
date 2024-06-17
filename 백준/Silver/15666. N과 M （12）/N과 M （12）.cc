e4e37d153b4e3f453150710ca254d7b902739bf6
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2024-06-17 11:07:06 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -1,44 +1,39 @@
-#include <iostream>
-#include <algorithm>
-
-#define MAX 9
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int N, M;
-int path[MAX];
-int tmp[MAX], arr[MAX], idx = 0;
+vector<int> v;
+vector<int> path;
 
-void DFS(int level, int start) {
-	if (level >= M) {
-		for (int i = 0; i < level; i++) {
-			printf("%d ", path[i]);
-		}
-		printf("\n");
+void run(int level, int start) {
+	if (level == M) {
+		for (int e : path) {
+			cout << e << ' ';
+		} cout << '\n';
 		return;
 	}
-	for (int t = start; t <= idx; t++) {
-		path[level] = arr[t];
-		DFS(level + 1, t);
-		path[level] = 0;
+	int last_num = 0;
+	for (int i = start; i < N; i++) {
+		if (last_num == v[i]) continue;
+		path.push_back(v[i]);
+		last_num = v[i];
+
+		run(level + 1, i);
+
+		path.pop_back();
 	}
 }
 
 int main() {
-	// freopen_s(new FILE*, "sample_input.txt", "r", stdin);
-	scanf("%d %d", &N, &M);
+	cin >> N >> M;
+	v.resize(N);
 	for (int i = 0; i < N; i++) {
-		scanf("%d", &tmp[i]);
+		cin >> v[i];
 	}
-	sort(tmp, tmp+ N);
-	arr[0] = tmp[0];
+	sort(v.begin(), v.end());
 
-	for (int i = 1; i < N; i++) {
-		if (tmp[i - 1] == tmp[i]) continue;
-		arr[++idx] = tmp[i];
-	}
-
-	DFS(0, 0);
+	run(0, 0);
 
 	return 0;
 }
