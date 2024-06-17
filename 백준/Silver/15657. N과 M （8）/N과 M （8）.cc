d794f88483f19eb2d3271fd3e16313cb68239fd9
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2024-06-17 10:29:10 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -1,22 +1,15 @@
 #include <iostream>
 #include <algorithm>
-#define MAX 9
+
 using namespace std;
 
 int N, M;
-int path[MAX];
-int arr[MAX];
-
-void Input() {
-	scanf("%d %d", &N, &M);
-	for (int i = 0; i < N; i++) {
-		scanf("%d", &arr[i]);
-	}
-	sort(arr, arr + N);
-}
+int arr[8];
+int path[8];
 
 void DFS(int level, int start) {
-	if (level >= M) {
+	// if (level >= 2 && path[level - 2] > path[level - 1]) return;
+	if (level == M) {
 		for (int i = 0; i < level; i++) {
 			printf("%d ", path[i]);
 		}
@@ -24,18 +17,24 @@ void DFS(int level, int start) {
 		return;
 	}
 
-	for (int t = start; t < N; t++) {
-		path[level] = arr[t];
-		DFS(level+1, t);
+	for (int x = start; x < N; x++) {
+
+		path[level] = arr[x];
+		DFS(level + 1, x);
 		path[level] = 0;
+
 	}
-	return;
+
+
 }
 
 int main() {
-	// freopen_s(new FILE*, "sample_input.txt", "r", stdin);
-	
-	Input();
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	sort(arr, arr + N);
 
 	DFS(0, 0);
 
