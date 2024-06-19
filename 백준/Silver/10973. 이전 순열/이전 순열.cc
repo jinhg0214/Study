cf5b4f1bc73a7aa0fbb872582cdf86a30ae415bc
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2024-06-19 21:33:12 +0900

    [Silver III] Title: 이전 순열, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10973. 이전 순열/이전 순열.cc
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -5,19 +5,31 @@
 using namespace std;
 
 int main() {
+	// freopen_s(new FILE*, "input.txt", "r", stdin);
+
 	int N; cin >> N;
 	vector<int> v(N);
 	for (int i = 0; i < N; i++)
 		cin >> v[i];
 	
-	if (prev_permutation(v.begin(), v.end())) {
-		for (int e : v) {
-			cout << e << ' ';
+	for (int i = N - 1; i > 0; i--) {
+		int x = v[i - 1];
+		int y = v[i];
+		if (x > y) {
+			for (int j = N - 1; j > 0; j--) {
+				if (x > v[j]) {
+					swap(v[i - 1], v[j]);
+					sort(v.begin() + i, v.end(), std::greater<int>());
+					for (int e : v) {
+						cout << e << ' ';
+					}
+					return 0;
+				}
+			}
 		}
 	}
-	else {
-		cout << -1;
-	}
+	cout << -1;
 
 	return 0;
 }
+
