932074f5c70ff59ad7035ebe5b2535cf1690ad72
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2024-06-19 21:01:24 +0900

    [Silver III] Title: 다음 순열, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.cc
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -5,26 +5,36 @@
 using namespace std;
 
 int main() {
-	int N;
-	cin >> N;
+	// freopen_s(new FILE*, "input.txt", "r", stdin);
+
+	int N; cin >> N;
 	vector<int> v(N);
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
 		cin >> v[i];
+
+
+	// 1. 뒤에서부터 두개씩 확인하면서, y가 x보다 큰 경우 찾기
+	// 2. x, y를 저장해두고, 다시 뒤에서부터 x보다 큰 값을 찾아 x와 swap
+	// 3. 이후 y부터 sort하기
+
+	int x, y;
+	for (int i = N - 1; i > 0; i--) {
+		x = v[i - 1];
+		y = v[i];
+		if (x < y) {
+			for (int j = N - 1; j > 0; j--) {
+				if (x < v[j]) {
+					swap(v[i-1], v[j]);
+					sort(v.begin() + i, v.end());
+					for (int e : v) {
+						cout << e << ' ';
+					}
+					return 0;
+				}			
+			}
+		}
 	}
-	vector<int> sorted = v;
-
-	sort(sorted.begin(), sorted.end());
-
-	next_permutation(v.begin(), v.end());
-
-	if (v == sorted) {
-		cout << "-1";
-		return 0;
-	}
-
-	for (int e : v) {
-		cout << e << ' ';
-	}	
+	cout << -1;
 
 	return 0;
 }
