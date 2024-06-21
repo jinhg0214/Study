d5331dbd24e6817435f9ecf4f7335277768e13c1
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2024-06-22 08:31:20 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.cc
+++ 백준/Silver/6603. 로또/로또.cc
@@ -1,64 +1,35 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int> lotto; // 선택한 번호 6개를 저장하는 배열
+int main(){
+	// freopen_s(new FILE*, "input.txt", "r", stdin);
 
-// a : 입력으로 주어진 수의 배열
-// index : 선택할지 말지 결정해야 하는 인덱스
-// cnt : 현재까지 포함된 개수
-void go(vector<int> & a, int index, int cnt) {
+	while (1) {
+		int k;	cin >> k;
+		if (k == 0) break;
+		vector<int> v(k);
+		for (int i = 0; i < k; i++)
+			cin >> v[i];
 
-	// case1 : 정답을 찾은경우 (6개를 고른경우)
-	if (cnt == 6) { 
-		for (int num : lotto) {
-			printf("%d ", num);
-		}
-		cout << '\n';
-		return;
-	}
+		vector<int> idx; // 조합을 위한 index 벡터. next_permutation으로 조합 생성용
+		for (int i = 0; i < 6; i++) // 0일떄 뽑는다
+			idx.push_back(0);
 
-	// case2 : 불가능한 경우
-	if (index >= a.size()) { // 더이상 선택할 수가 없는 경우
-		return;
-	}
+		for (int i = 0; i < k - 6; i++) // 1일때 거른다
+			idx.push_back(1);
 
-	// case3 : 다음 경우
-	// 1) 인덱스를 포함하여 다음 경우 호출
-	lotto.push_back(a[index]); // 수를 선택했으므로 lotto 배열에 추가
-	go(a, index + 1, cnt + 1);
-	lotto.pop_back();	// 다음 경우를 위해 index번째를 선택한 경우는 lotto에서 뺌
+		// 조합 생성
+		do {
+			for (int i = 0; i<v.size(); i++) {
+				if (idx[i] == 0) { 
+					cout << v[i] << ' ';
+				}
+			} cout << '\n';
 
-	// 2) 선택하지 않고 다음 경우 호출
-	go(a, index + 1, cnt);
-
-}
-
-int main() {
-
-	while (true) {
-		int k; // 개수
-
-		cin >> k;
-		if (k == 0) {
-			break;
-		}
-
-		vector<int> s(k);
-
-		for (int i = 0; i < k; i++) {
-			cin >> s[i];
-		}
-
-		// 사전순으로 출력을 위해 정렬
-		sort(s.begin(), s.end());
-
-		go(s, 0, 0);
-
+		} while (next_permutation(idx.begin(),idx.end()));
 		cout << '\n';
 	}
-
+	
 	return 0;
 }
