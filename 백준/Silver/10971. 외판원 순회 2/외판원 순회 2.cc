9d1a4395099b92c8b84d89b52a8c8d9e40325317
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2024-06-21 18:59:56 +0900

    [Silver II] Title: 외판원 순회 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.cc
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.cc
@@ -1,55 +1,53 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int N;
-
 int map[11][11];
-int path[11];
 int visited[11];
-int res = 21e8;
+int min_cost = 21e8;
 
-void Input() {
-	cin >> N;
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < N; x++) {
-			cin >> map[y][x];
-		}
-	}
-}
+// 첫 출발 도시, 현재 도시, 몇번째 경로, 현재까지 비용을 받음
+void run(int start, int now, int level, int cost) {
+	if (cost > min_cost) return; // 가지치기 : 이미 최소 비용을 초과한 경우 더이상 보지 않음
+	if (level == N-1) { // 모든 도시를 방문한 경우
+		
+		// 집으로 돌아갈 경로가 없다면 리턴
+		if (map[now][start] == 0) return;
 
-void path_find(int start, int y, int sum,int cnt) {
-	// 이미 최소값을 초과한 경우 패스
-	if (sum > res) return;
-	// 모든 도시를 들렀고, 다시 시작점으로 돌아온 경우.
-	if (cnt == N && start == y) {
-		if (sum < res) {
-			res = sum;
-		}
+		cost += map[now][start]; // 마지막으로 원래의 도시까지 돌아오는 코스트 계산
+
+		min_cost = min(min_cost, cost);
 		return;
 	}
 
-	for (int x = 0; x < N; x++) {
-		if (map[y][x] == 0) continue;
-		if (visited[x] == 1) continue;
+	for (int i = 0; i < N; i++) {
+		if (map[now][i] == 0) continue; 
+		if (visited[i] == 1) continue;
 
-		visited[x] = 1;
-		path_find(start, x, sum + map[y][x], cnt + 1);
-		visited[x] = 0;
+		visited[i] = 1;
+		run(start, i, level + 1, cost + map[now][i]);
+		visited[i] = 0;
 	}
-
 }
 
 int main() {
 	// freopen_s(new FILE*, "input.txt", "r", stdin);
-	Input();
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+		}
+	}
 	
 	for (int i = 0; i < N; i++) {
-		path_find(i, i, 0, 0);
+		visited[i] = 1;
+		run(i, i, 0, 0);
+		visited[i] = 0;
 	}
-
-	cout << res;
+	
+	cout << min_cost;
 
 	return 0;
 }
