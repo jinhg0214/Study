21f014a71c9ae1429a4d4a4716ebca138e9eb9b4
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2024-07-03 16:41:29 +0900

    [Silver V] Title: 집합, Time: 588 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,70 +1,48 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
+bool arr[21];
+
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	//freopen_s(new FILE*, "input.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-	int M, N;
-	unsigned int s = 0;
-	string cmd;
-
-	cin >> M;
+	// freopen_s(new FILE*, "input.txt", "r", stdin);
+	int M; cin >> M;
 	while (M--) {
+		string cmd;
+		int x;
 		cin >> cmd;
+
 		if (cmd == "add") {
-			cin >> N;
-			s |= (1 << N); // 원소 추가
+			cin >> x;
+			arr[x] = true;
 		}
 		else if (cmd == "remove") {
-			cin >> N;
-			s &= ~(1 << N); // 원소 제거
+			cin >> x;
+			arr[x] = false;
 		}
 		else if (cmd == "check") {
-			cin >> N;
-			if (s & (1 << N)) {
-				cout << 1 << '\n';
-			}
-			else {
-				cout << 0 << '\n';
-			}
+			cin >> x;
+			cout << arr[x] << '\n';
 		}
 		else if (cmd == "toggle") {
-			cin >> N;
-			s ^= (1 << N);
+			cin >> x;
+			arr[x] = !arr[x];
 		}
 		else if (cmd == "all") {
-			s = (1 << 21) - 1; // 모두 1로 설정
+			for (int i = 1; i <= 20; i++) {
+				arr[i] = true;
+			}
 		}
 		else if (cmd == "empty") {
-			s = 0;
+			for (int i = 1; i <= 20; i++) {
+				arr[i] = false;
+			}
 		}
+
 	}
+
 	return 0;
-
-
-	/*
-	입력 :
-	첫째 줄에 수행해야 하는 연산의 수 M (1 ≤ M ≤ 3,000,000)이 주어진다.
-
-	둘째 줄부터 M개의 줄에 수행해야 하는 연산이 한 줄에 하나씩 주어진다.
-
-	add x: S에 x를 추가한다. (1 ≤ x ≤ 20) S에 x가 이미 있는 경우에는 연산을 무시한다.
-	remove x: S에서 x를 제거한다. (1 ≤ x ≤ 20) S에 x가 없는 경우에는 연산을 무시한다.
-	check x: S에 x가 있으면 1을, 없으면 0을 출력한다. (1 ≤ x ≤ 20)
-	toggle x: S에 x가 있으면 x를 제거하고, 없으면 x를 추가한다. (1 ≤ x ≤ 20)
-	all: S를 {1, 2, ..., 20} 으로 바꾼다.
-	empty: S를 공집합으로 바꾼다.
-
-	출력 :
-	check 연산이 주어질때 마다 결과를 출력.
-
-	풀이 :
-		int[21] 배열을 사용했으나 시간 초과로 실패
-		unsigned int의 숫자를 비트마스킹하여 풀이하는 방식으로 해결.
-		비트연산자의 사용법 한번 정리해둘 것
-	*/
 }
