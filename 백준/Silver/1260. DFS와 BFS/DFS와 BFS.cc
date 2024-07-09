e4e4b887c2ab92e852195635799ec70e9c31cfe4
Jin Haeng Lee <70011316+jinhg0214@users.noreply.github.com>
2024-07-09 09:41:34 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,78 +1,74 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int N, M, V;
-vector< vector<int> > map;
-int visited[1001] = { 0 };
+vector<vector<int>> edge;
+bool visited[1001];
 
-void DFS(int now) {
-	cout << now << " ";
+void input() {
+	// freopen_s(new FILE*, "input.txt", "r", stdin);
+	cin >> N >> M >> V;
 
-	// 현재 노드와 연결된 노드 찾기
-	for (int i = 0; i < map[now].size(); i++) {
-		int next = map[now][i];
-		if (visited[next] == 1) continue;
-		visited[next] = 1;
-		DFS(next);
+	edge.resize(N + 1);
+	for (int i = 0; i < M; i++) {
+		int start, end;
+		cin >> start >> end;
+		edge[start].push_back(end);
+		edge[end].push_back(start);
 	}
 
+	// 엣지가 여러개인 경우 노드가 작은것부터 방문
+	for (int i = 0; i <= N; i++) {
+		sort(edge[i].begin(), edge[i].end());
+	}
 }
 
-void BFS(int start) {
-	queue<int> qu;
+void dfs(int level, int cur) {
+	// 들어오면서 방문 처리 후 출력
+	visited[cur] = 1;
+	cout << cur << ' ';
 
+	for (int i = 0; i < edge[cur].size(); i++) {
+		int next = edge[cur][i];
+
+		if (visited[next] == 1) continue;
+		dfs(level + 1, next);
+	}
+}
+
+void bfs() {
+	memset(visited, 0, sizeof(visited));
+
+	queue<int> qu;
 	visited[V] = 1;
-	qu.push(start);
+	qu.push(V);
 
 	while (!qu.empty()) {
 		int now = qu.front();
 		qu.pop();
 
-		cout << now << " ";
+		cout << now << ' ';
 
-		for (int i = 0; i < map[now].size(); i++) {
-			int next = map[now][i];
+		for (int i = 0; i < edge[now].size(); i++) {
+			int next = edge[now][i];
 			if (visited[next] == 1) continue;
 			visited[next] = 1;
 			qu.push(next);
 		}
-
 	}
 
-
+	return;
 }
 
 int main() {
-	// freopen_s(new FILE*, "input.txt", "r", stdin);
+	input();
 
-	cin >> N >> M >> V;
+	dfs(0, V);
 
-	map.resize(N + 1);
+	cout << '\n';
 
-	while (M--) {
-		int start, end;
-		cin >> start >> end;
-		map[start].push_back(end);
-		map[end].push_back(start);
-	}
-
-	for (int i = 0; i < N + 1; i++) {
-		sort(map[i].begin(), map[i].end());
-	}
-	
-	
-	
-	visited[V] = 1;
-	DFS(V);
-	cout << "\n";
-
-	fill(visited, visited + 1001, 0);
-
-	BFS(V);
+	bfs();
 
 	return 0;
 }
